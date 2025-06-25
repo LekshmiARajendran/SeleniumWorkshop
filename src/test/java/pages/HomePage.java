04863ddd32785efbb322ed694b6b61e48e0cbaa8
lanijarajendrannair@eurofunk.com <lanijarajendrannair@eurofunk.com>
2025-06-25 16:20:00 +0200

    Added new Selenium test cases

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,14 +6,32 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
 
     private WebDriver driver;
-    private By formAuthenticationLink = By.linkText("Form Authentication");
+    //private By formAuthenticationLink = By.linkText();
     //creating a Constructor for the class
     public HomePage(WebDriver driver){
         this.driver = driver;
     }
     public LoginPage clickFormAuthentication(){
-        driver.findElement(formAuthenticationLink).click();
+        clickLink("Form Authentication");
         return new LoginPage(driver);
-
     }
+    //dropdown page
+    public DropDownPage clickDropDown(){
+        clickLink("Dropdown");
+        return new DropDownPage(driver);
+    }
+    //generic way to return all the links
+    private void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+    }
+    public ForgotPasswordPage clickForgotPassword(){
+        clickLink("Forgot Password");
+        return new ForgotPasswordPage(driver);
+    }
+    //Hover menu
+    public HoversPage clickHovers(){
+        clickLink("Hovers");
+        return new HoversPage(driver);
+    }
+
 }
